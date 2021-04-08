f9436c13bb4f4e034e2f8786decb121b85377918
liweiyap <liweiyap@gmail.com>
2021-04-08 13:02:14 +0800

    Swap avatars for hover and unhover functions

--- assets/js/switch-avatar.js
+++ assets/js/switch-avatar.js
@@ -1,11 +1,11 @@
 /* https://stackoverflow.com/a/18032363/12367873 */
 
-function hover(sidebarAvatar) {
+function unhover(sidebarAvatar) {
     sidebarAvatar.setAttribute('src', '../assets/logo.svg');
     sidebarAvatar.setAttribute('style', 'transition: 0.3s;');
 }
 
-function unhover(sidebarAvatar) {
+function hover(sidebarAvatar) {
     sidebarAvatar.setAttribute('src', '../assets/liwei.jpg');
     sidebarAvatar.setAttribute('style', 'border-radius: 50%; transition: 0.3s;');
 }
