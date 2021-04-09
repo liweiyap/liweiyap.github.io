7297cbdd6f538ed9b23c3ade376face8e87d902d
liweiyap <liweiyap@gmail.com>
2021-04-09 15:09:57 +0800

    Complete German translations for Nebenprojekte. Add carousel. Change logo colour.

--- assets/js/switch-avatar.js
+++ assets/js/switch-avatar.js
@@ -1,11 +1,11 @@
 /* https://stackoverflow.com/a/18032363/12367873 */
 
 function unhover(sidebarAvatar) {
-    sidebarAvatar.setAttribute('src', '../assets/logo.svg');
+    sidebarAvatar.setAttribute('src', '../assets/logo-whitesmoke.svg');
     sidebarAvatar.setAttribute('style', 'transition: 0.3s;');
 }
 
 function hover(sidebarAvatar) {
     sidebarAvatar.setAttribute('src', '../assets/liwei.jpg');
-    sidebarAvatar.setAttribute('style', 'border-radius: 50%; transition: 0.3s;');
+    sidebarAvatar.setAttribute('style', 'border-radius: 50%; opacity: 1.0; transition: 0.3s;');
 }
