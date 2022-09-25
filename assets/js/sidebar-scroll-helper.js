4d83c42a2353a31fce37439e16122ca6ae4797c7
Li-Wei Yap <liweiyap@gmail.com>
2022-09-25 14:18:13 +0200

    Experiment with touchmove and touchcancel in sidebar

--- assets/js/sidebar-scroll-helper.js
+++ assets/js/sidebar-scroll-helper.js
@@ -6,6 +6,16 @@ elementSidebar.addEventListener('touchstart', function (event) {
     elementSidebar.style.overflowY = "auto";
 }, false);
 
+elementSidebar.addEventListener('touchmove', function (event) {
+    if ((elementSidebar.scrollTop === 0) || (elementSidebar.scrollTop === elementSidebar.scrollHeight - elementSidebar.offsetHeight)) {
+        elementSidebar.style.overflow = "hidden";
+    }
+}, false);
+
 elementSidebar.addEventListener('touchend', function (event) {
+    elementSidebar.style.overflow = "hidden";
+}, false);
+
+elementSidebar.addEventListener('touchcancel', function (event) {
     elementSidebar.style.overflow = "hidden";
 }, false);
